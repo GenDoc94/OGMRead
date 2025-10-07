6240c9a0da32b0926e7ba080dbd9bbfcb15ec593
GenDoc94 <juanjose.dominguez@scsalud.es>
2025-10-07 20:41:18 +0200

    adding info from bionapp

--- read_files.R
+++ read_files.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(readxl)
 
 #List all files that start with number
 archives <- list.files(
@@ -8,7 +9,7 @@ archives <- list.files(
 )
 
 #Read all and add column Id
-all_data <- archives %>%
+all_variants <- archives %>%
         set_names() %>%  #keep the name of the archive
         map_dfr(~ {
                 read_tsv(.x, na = c("null", "-", "NA"), comment = "") %>%
@@ -18,12 +19,37 @@ all_data <- archives %>%
                 across(c(Classification, Type, Zygosity), as.factor)
         )
 
-#Nested base
-n_data <- all_data |> group_by(Id) |> nest()
 
+ddx <- read_tsv("bionapp_files/DDx.txt",
+                col_names = TRUE,
+                locale = locale(encoding = "Latin1")) %>%
+        mutate(label=Dx) %>% select(-Dx) #rename doesnt work
+
+dmuestra <- read_tsv("bionapp_files/DMuestra.txt",
+                     col_names = TRUE,
+                     locale = locale(encoding = "Latin1"))
+
+all_metadata <- read_tsv("bionapp_files/Muestras.txt",
+                         col_names = TRUE,
+                         locale = locale(encoding = "Latin1")) %>% 
+        mutate(Id=NumBN, .before=1) %>% select(-NumBN) %>% #rename doesnt work
+        left_join(ddx, by = c("Dx" = "Cod")) %>%
+        mutate(Dx = label) %>%
+        select(-label) %>% mutate(Dx = as.factor(Dx)) %>%
+        left_join(dmuestra, by = c("Muestra" = "Cod")) %>%
+        mutate(Muestra = TipoMuestra) %>%
+        select(-TipoMuestra) %>% mutate(Muestra = as.factor(Muestra))
+
+rm(archives, ddx, dmuestra)
+
+#Nested base
+n_variant <- all_variants |> group_by(Id) |> nest()
+
+#Mixing data with metadata (just keeping data)
+n_variant_complete <- left_join(n_variant,all_metadata, by="Id")
 
 #Filtering P/LP/VUS variants
-n_f_data <- n_data %>%
+n_variant_filter <- n_variant %>%
         mutate(
                 data = map(data, ~ filter(.x, Classification %in% c("Uncertain significance", 
                                                                     "Likely pathogenic", 
